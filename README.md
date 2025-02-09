c55dd4883cd7e2e4792ab15643b1c8f90ceb41b6
Enio Aiello <aielloenio@icloud.com>
2025-02-09 18:10:45 +0100

    Application de base

---
+++ README.md
@@ -0,0 +1,9 @@
+![capture](#)
+
+# ⚡️ Quick
+
+**Quick** permet aux utilisateurs de télécharger des applications à la manière de [macapps](https://macapps.link) via [brew](https://brew.sh).
+
+## 🔰 Utilisation
+
+Rendez-vous simplement sur [cette page](#), choisissez les applications que vous souhaitez installer, copiez la commande qui s'affiche, entrez-la dans votre terminal et voilà !
---
+++ assets/js/script.js
@@ -0,0 +1,105 @@
+// Sélection de l'élément où afficher les applications
+const applicationsContainer = document.querySelector(".applications-container");
+let selectedApplications = [];
+const errorAlert = document.querySelector(".error");
+const modal = document.querySelector('#installModal');
+const helpButton = document.querySelector(".help");
+const outputCommand = document.querySelector(".command");
+
+// Sélectionner les applications à installer
+function selectApp(card, appId) {
+    const index = selectedApplications.indexOf(appId);
+
+    if (index === -1) {
+        // Ajouter l'application si elle n'est pas déjà sélectionnée
+        selectedApplications.push(appId);
+        card.classList.add("text-bg-primary");
+    } else {
+        // Retirer l'application si elle est déjà sélectionnée
+        selectedApplications.splice(index, 1);
+        card.classList.remove("text-bg-primary");
+    }
+}
+
+// Générer la ligne de commande brew qui va permettre d'installer les applications
+function generateInstallCommand() {
+    if (selectedApplications.length === 0) {
+        errorAlert.style.display = "block";
+        errorAlert.style.animation = "displayError 5s ease-in-out";
+        setTimeout(() => {
+            errorAlert.style.display = "none";
+        }, 5000);
+    } else {
+        // Afficher le modal avec Bootstrap
+        const bootstrapModal = new bootstrap.Modal(modal);
+        bootstrapModal.show();
+    }
+}
+
+// Détecter la touche "i" pour ouvrir le modal
+document.addEventListener("keydown", (event) => {
+    if (event.key === "i" || event.key === "I") {
+        // Générer et afficher la commande
+        outputCommand.textContent = "brew install --cask " + selectedApplications.join(" ");
+
+        generateInstallCommand();
+    }
+});
+
+// Fonctionnement du bouton d'aide
+helpButton.addEventListener("click", () => {
+    window.open('https://corundum.gitbook.io/quick', '_blank');
+});
+
+// Chargement du fichier JSON avec fetch()
+fetch("http://localhost:8888/quick/assets/json/applications.json")
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Erreur lors du chargement du fichier JSON");
+        }
+        return response.json();
+    })
+    .then(data => {
+        // Effacer le contenu existant avant d'ajouter les nouvelles cartes
+        applicationsContainer.innerHTML = "";
+
+        // Boucle sur les données JSON pour générer les cartes dynamiquement
+        data.forEach(app => {
+            // Créer les éléments
+            const colDiv = document.createElement("div");
+            colDiv.classList.add("col");
+
+            const cardDiv = document.createElement("div");
+            cardDiv.classList.add("card", "h-100");
+            cardDiv.addEventListener("click", () => selectApp(cardDiv, app.id));
+
+            const imgContainer = document.createElement("div");
+            imgContainer.classList.add("w-100", "d-flex", "justify-content-center", "align-items-center", "mt-4");
+
+            const img = document.createElement("img");
+            img.src = app.icon;
+            img.classList.add("w-25");
+            img.alt = app.name;
+
+            const cardBody = document.createElement("div");
+            cardBody.classList.add("card-body");
+
+            const cardTitle = document.createElement("h5");
+            cardTitle.classList.add("card-title", "text-center");
+            cardTitle.textContent = app.name;
+
+            const cardText = document.createElement("p");
+            cardText.classList.add("card-text", "text-muted", "text-center");
+            cardText.textContent = app.description;
+
+            // Assembler les éléments
+            imgContainer.appendChild(img);
+            cardBody.appendChild(cardTitle);
+            cardBody.appendChild(cardText);
+            cardDiv.appendChild(imgContainer);
+            cardDiv.appendChild(cardBody);
+            colDiv.appendChild(cardDiv);
+            applicationsContainer.appendChild(colDiv);
+        });
+    })
+    .catch(error => console.error("Erreur :", error));
