a4981537897f798c9444adf12180f88109422da1
Enio Aiello <aielloenio@icloud.com>
2025-03-19 15:08:39 +0100

    Modification des raccourcis

--- assets/js/applications.js
+++ assets/js/applications.js
@@ -1,11 +1,14 @@
 // Sélection de l'élément où afficher les applications
-const applicationsContainer = document.querySelector(".applications-container");
+const applicationsContainer = document.querySelector("#applications-container");
+const selectedApplicationContainer = document.querySelector("#application-detail");
 let selectedApplications = [];
 const errorAlert = document.querySelector(".error");
 const modal = document.querySelector('#installModal');
 const helpButton = document.querySelector(".help");
 const outputCommand = document.querySelector(".command");
 const status = document.querySelector(".status");
+const searchBox = document.querySelector("#search");
+const searchInput = document.querySelector("#search-input");
 let isDisplayed = false;
 
 // Sélectionner les applications à installer
@@ -23,6 +26,39 @@ function selectApp(card, appId) {
     }
 }
 
+// Afficher les détails d'une application
+function appDetails(appId) {
+    fetch("https://corundumproject.github.io/quick/assets/json/applications.json")
+        .then(response => response.json())
+        .then(data => {
+            const app = data.find(app => app.id === appId);
+            if (app) {
+                selectedApplicationContainer.innerHTML = `
+                    <div class="card">
+                        <div class="card-body text-center">
+                            <img src="${app.icon}" alt="${app.name}" class="w-25 mb-3 text-center">
+                            <h5 class="card-title text-center">${app.name}</h5>
+                            <h6 class="card-subtitle mb-2 text-muted text-center">${app.category}</h6>
+                            <p class="card-text text-center">${app.description}</p>
+                            <a href="${app.website}" target="_blank" class="btn btn-primary">Site officiel</a>
+                            <button id="back-button" class="btn btn-secondary">Revenir en arrière</button>
+                        </div>
+                    </div>
+                `;
+                applicationsContainer.classList.add("d-none");
+                searchBox.classList.add("d-none");
+                document.querySelector("#back-button").addEventListener("click", hideDetails);
+            }
+        })
+        .catch(error => console.error("Erreur :", error));
+}
+
+function hideDetails() {
+    selectedApplicationContainer.innerHTML = ``;
+    applicationsContainer.classList.remove("d-none");
+    searchBox.classList.remove("d-none");
+}
+
 // Générer la ligne de commande brew qui va permettre d'installer les applications
 function generateInstallCommand() {
     if (selectedApplications.length === 0) {
@@ -38,17 +74,6 @@ function generateInstallCommand() {
     }
 }
 
-// Détecter la touche "i" pour ouvrir le modal
-document.addEventListener("keydown", (event) => {
-    if (event.key === "i" || event.key === "I") {
-        // Générer et afficher la commande
-        const command = "brew install --cask " + selectedApplications.join(" ");
-        outputCommand.textContent = command;
-
-        generateInstallCommand();
-    }
-});
-
 // Fonctionnement du bouton d'aide
 helpButton.addEventListener("click", () => {
     window.open('https://corundum.gitbook.io/quick', '_blank');
@@ -62,6 +87,7 @@ outputCommand.addEventListener("click", () => {
     navigator.clipboard.writeText(commandText).then(() => {
         status.classList.add("text-success");
         status.textContent = "Texte copié !";
+
         setTimeout(() => {
             status.classList.remove("text-success");
             status.textContent = "";
@@ -76,65 +102,115 @@ outputCommand.addEventListener("click", () => {
     });
 });
 
-// Chargement du fichier JSON avec fetch()
-fetch("https://corundumproject.github.io/quick/assets/json/applications.json")
-    .then(response => {
-        if (!response.ok) {
-            throw new Error("Erreur lors du chargement du fichier JSON");
-        }
-        return response.json();
-    })
-    .then(data => {
-        // Effacer le contenu existant avant d'ajouter les nouvelles cartes
-        applicationsContainer.innerHTML = "";
+document.addEventListener("DOMContentLoaded", () => {
+    // Chargement du fichier JSON avec fetch()
+    const applicationsContainer = document.getElementById("applications-container");
+    const searchInput = document.getElementById("search-input");
+    const searchButton = document.getElementById("search-button");
 
-        // Boucle sur les données JSON pour générer les cartes dynamiquement
-        data.forEach(app => {
-            // Créer les éléments
-            const colDiv = document.createElement("div");
-            colDiv.classList.add("col");
+    function fetchAndDisplayApps(searchTerm = "") {
+        fetch("https://corundumproject.github.io/quick/assets/json/applications.json")
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("Erreur lors du chargement du fichier JSON");
+                }
+                return response.json();
+            })
+            .then(data => {
+                applicationsContainer.innerHTML = ""; // Effacer les résultats précédents
+                data.filter(app => {
+                    const lowerSearch = searchTerm.toLowerCase();
+                    return (
+                        app.name.toLowerCase().includes(lowerSearch) ||
+                        app.category.toLowerCase().includes(lowerSearch) ||
+                        app.developer.toLowerCase().includes(lowerSearch)
+                    );
+                }).forEach(app => {
+                    const colDiv = document.createElement("div");
+                    colDiv.classList.add("col-md-4", "mb-3");
 
-            const cardDiv = document.createElement("div");
-            cardDiv.classList.add("card", "h-100", "border-3");
-            cardDiv.addEventListener("mousedown", () => selectApp(cardDiv, app.id));
+                    const cardDiv = document.createElement("div");
+                    cardDiv.classList.add("card", "h-100", "border-3");
+                    cardDiv.addEventListener("click", () => selectApp(cardDiv, app.id));
 
-            const imgContainer = document.createElement("div");
-            imgContainer.classList.add("w-100", "d-flex", "justify-content-center", "align-items-center", "mt-4", "pe-auto");
+                    if (selectedApplications.includes(app.id)) {
+                        cardDiv.classList.add("border-primary");
+                    }
 
-            const img = document.createElement("img");
-            img.src = app.icon;
-            img.classList.add("w-25");
-            img.alt = app.name;
+                    const imgContainer = document.createElement("div");
+                    imgContainer.classList.add("w-100", "d-flex", "justify-content-center", "align-items-center", "mt-4");
 
-            const cardBody = document.createElement("div");
-            cardBody.classList.add("card-body");
+                    const img = document.createElement("img");
+                    img.src = app.icon;
+                    img.classList.add("w-25");
+                    img.alt = app.name;
 
-            const cardTitle = document.createElement("h5");
-            cardTitle.classList.add("card-title", "text-center");
-            cardTitle.textContent = app.name;
+                    const cardBody = document.createElement("div");
+                    cardBody.classList.add("card-body");
 
-            const cardSubtitle = document.createElement("h6");
-            cardSubtitle.classList.add("card-subtitle", "text-center", "text-muted", "mt-2", "mb-2");
-            cardSubtitle.textContent = app.category;
+                    const cardTitle = document.createElement("h5");
+                    cardTitle.classList.add("card-title", "text-center");
+                    cardTitle.textContent = app.name;
 
-            const cardText = document.createElement("p");
-            cardText.classList.add("card-text", "text-center", "text-muted");
-            cardText.textContent = app.description;
+                    const cardLink = document.createElement("button");
+                    cardLink.classList.add("btn", "btn-primary", "mt-2", "mb-2", "w-100");
+                    cardLink.textContent = "Détails";
+                    cardLink.addEventListener("click", (event) => {
+                        event.stopPropagation(); // Empêcher la propagation de l'événement
+                        appDetails(app.id);
+                    });
 
-            const cardInformation = document.createElement("a");
-            cardInformation.classList.add("card-link");
-            cardInformation.innerHTML = "<i class='bi bi-info-circle-fill'></i>";
+                    imgContainer.appendChild(img);
+                    cardBody.appendChild(cardTitle);
+                    cardDiv.appendChild(imgContainer);
+                    cardDiv.appendChild(cardBody);
+                    cardBody.appendChild(cardLink);
+                    colDiv.appendChild(cardDiv);
+                    applicationsContainer.appendChild(colDiv);
+                });
+            })
+            .catch(error => console.error("Erreur :", error));
+    }
 
-            // Assembler les éléments
-            imgContainer.appendChild(img);
-            cardBody.appendChild(cardTitle);
-            // cardBody.appendChild(cardSubtitle);
-            // cardBody.appendChild(cardText);
-            // cardBody.appendChild(cardInformation);
-            cardDiv.appendChild(imgContainer);
-            cardDiv.appendChild(cardBody);
-            colDiv.appendChild(cardDiv);
-            applicationsContainer.appendChild(colDiv);
-        });
-    })
-    .catch(error => console.error("Erreur :", error));
+    searchButton.addEventListener("click", () => {
+        fetchAndDisplayApps(searchInput.value);
+    });
+
+    searchInput.addEventListener("input", () => {
+        fetchAndDisplayApps(searchInput.value);
+    });
+
+    fetchAndDisplayApps();
+});
+
+// Gestion des événements clavier
+function handleKeyPress(event) {
+    if (event.key === "i" || event.key === "I") {
+        // Générer et afficher la commande
+        const command = "brew install --cask " + selectedApplications.join(" ");
+        outputCommand.textContent = command;
+        generateInstallCommand();
+    }
+}
+
+// Activer/Désactiver les événements clavier
+function enableKeyListeners() {
+    document.addEventListener("keydown", handleKeyPress);
+}
+
+function disableKeyListeners() {
+    document.removeEventListener("keydown", handleKeyPress);
+}
+
+// Écoute des événements de la barre de recherche
+searchInput.addEventListener("focus", () => {
+    disableKeyListeners();
+});
+
+searchInput.addEventListener("blur", () => {
+    enableKeyListeners();
+});
+
+
+// Activation initiale des écouteurs de touches
+enableKeyListeners();
--- assets/js/tooltips.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-    let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-        return new bootstrap.Tooltip(tooltipTriggerEl);
-    });
-});
